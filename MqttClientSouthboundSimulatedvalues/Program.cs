7b80c903549c0f8d3012d0c3389f24e7484c21d2
Rafiul Islam <rafiul.islam@ovgu.de>
2023-01-21 20:48:58 +0100

    Initital commit

---
+++ MqttClientSouthboundSimulatedvalues/Program.cs
@@ -0,0 +1,62 @@
+﻿using MqttClientSouthboundSimulatedvalues;
+
+using Newtonsoft.Json;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using uPLibrary.Networking.M2Mqtt;
+
+class Program
+{
+
+    // Main Method
+   static MqttClient client ;
+    static public void Main(String[] args)
+    {
+        // create client instance and connect to publicly hosted broker
+       client = new uPLibrary.Networking.M2Mqtt.MqttClient("test.mosquitto.org");
+       string clientId = Guid.NewGuid().ToString();
+      client.Connect(clientId);
+       String topic = "MacnineData/ID-0000";
+
+        RandomNumberGernarator rn = new RandomNumberGernarator();
+        
+        while (true)
+        {
+            double Tval = rn.Getrandomvalues(10,35);
+
+            string Temp = String.Format("{0:0.00}", Tval);
+
+            double Hval = rn.Getrandomvalueshumidity(22, 50);
+            String Humidity = String.Format("{0:0.00}", Hval);
+
+            double Spindlespeed = rn.GetSpindlespeed(0, 50);
+            String speed = String.Format("{0:0.00}", Spindlespeed);
+
+            var jsonObject = new
+            {
+                Temperature = Temp,
+                Humidity = Humidity,
+                Speed = speed,
+                Timestamp = DateTime.Now.ToString()
+            };
+
+            // convert JSON object to string
+            string data = JsonConvert.SerializeObject(jsonObject);
+
+            // print JSON string
+
+            Console.WriteLine("Temperature : " + Temp);
+            Console.WriteLine("Humidity : " + Humidity);
+            Console.WriteLine("Spindle Speed : " + speed);
+            Console.WriteLine("Timestamp : " + jsonObject.Timestamp);           
+            client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(data));
+
+           Console.WriteLine("Data published to topic: " + topic + " with payload: " + data);
+           // Console.ReadLine();
+            System.Threading.Thread.Sleep(1000);
+
+            
+
+        }
+    }
+}
---
+++ MqttClientSouthboundSimulatedvalues/RandomNumberGernarator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttClientSouthboundSimulatedvalues
+{
+    public class RandomNumberGernarator
+    {
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Speed { get; set; }   
+        public double Getrandomvalues(double rangemin, double rangemax)
+        {
+            Random random = new Random();
+            
+            Temperature = random.NextDouble()*(rangemax- rangemin) + rangemin;
+            return Temperature;  
+        }
+        public double Getrandomvalueshumidity(double rangemin, double rangemax)
+        {
+            Random random = new Random();
+
+            Humidity = random.NextDouble() * (rangemax - rangemin) + rangemin;
+            return Humidity;
+        }
+
+        public double GetSpindlespeed(double rangemin, double rangemax)
+        {
+            Random random = new Random();
+
+            Speed = random.NextDouble() * (rangemax - rangemin) + rangemin;
+            return Speed;
+        }
+
+
+    }
+}
